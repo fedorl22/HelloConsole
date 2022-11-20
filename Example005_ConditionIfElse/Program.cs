825ca2b90b9a8686e7ca857c194a1894862d7acb
Fedor <fdr@mail.ru>
2022-11-20 23:29:27 +0300

    add If Else Tolover

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+Console.WriteLine("Охренительное счастье привалило - Маша");
+} 
+else 
+{
+Console.WriteLine("Привет, "); 
+Console.WriteLine(username);  
+}
